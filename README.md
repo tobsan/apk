e798dca5fdef32c5f0b4d67616a5383696325106
Tobias Olausson <tobsan@tobsan.se>
2018-06-06 00:19:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# APK is Alkohol Per Krona
+APK translated is alcohol per SEK.
+
+The APK can easily be calculated because Sweden has an alcohol monopoly, meaning there is a single
+supplier, called Systembolaget, for any drinks with an alcohol amount above 3.5 percent by volume.
+
+This software uses the open data that Systembolaget supplies, and calculates how the alcohol amount
+relates to the price and volume of the product.
+
+# License and Copyright
+Copyright &copy; 2018 Tobias Olausson.
+
+This software is under AGPL 3.0, see the LICENSE file. Please note that the use of the open data by
+Systembolaget comes with certain restrictions, see https://www.systembolaget.se/api/.
---
+++ apk.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+#
+# Main file for APK (alcohol per krona) calculation.
+# 
+# Copyright (c) 2018 Tobias Olausson
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import xml.etree.ElementTree as ET
+import os.path
+import urllib.request
+
+def parse_alcohol(alc_as_string):
+    if not alc_as_string[-1:] == "%":
+        raise ValueError("Not an alcohol percentage, need to end with %")
+
+    alc_numbers = alc_as_string[:-1]
+    if not all(dig.isdigit() or dig == "." for dig in alc_numbers):
+        raise ValueError("Only digits and . allowed in alcohol percentage")
+
+    value = float(alc_numbers)
+    if value > 100:
+        raise ValueError("Percentage can't be larger than 100%")
+
+    return value
+
+products_file_name = "products.xml"
+products_file_url = "https://www.systembolaget.se/api/assortment/products/xml"
+
+if not os.path.exists(products_file_name):
+    print("No product database file available, downloading...")
+    urllib.request.urlretrieve(products_file_url, products_file_name)
+
+# TODO: Add attribute of when the file was last downloaded, and update if necessary
+tree = ET.parse(products_file_name)
+root = tree.getroot()
+
+apk_values = []
+for artikel in root.findall('.//artikel'):
+    nr = artikel.find('nr').text
+    name = artikel.find('Namn').text
+    cost = float(artikel.find('Prisinklmoms').text)
+    alcohol_percent = parse_alcohol(artikel.find('Alkoholhalt').text)
+
+    volume = float(artikel.find('Volymiml').text)
+    alcohol_ml = volume * (alcohol_percent / 100)
+    apk = alcohol_ml / cost
+
+    # TODO: Save the APK as an XML tag instead, so it can be easily used with other properties of
+    # that specific product
+    apk_values.append((nr, name, apk))
+
+#
+# The printout below is just for testing the apk calculation
+#
+
+print("*** Highest APK products! ***")
+# TODO: Cache the results here, store this list to file as well.
+apk_values.sort(key=lambda tup: tup[2], reverse=True)
+for art in apk_values[:20]:
+    print("apk of {} (# {}) is {}, https://www.systembolaget.se/{}".format(art[1], art[0], art[2], art[0]))
+
+print("*** Lowest APK products! ***")
+for art in apk_values[-20:]:
+    print("apk of {} (# {}) is {}, https://www.systembolaget.se/{}".format(art[1], art[0], art[2], art[0]))
